4810af9b34eb3a5eb5d1ed1cf4293f9b0352c042
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-09-20 12:33:39 -0300

    cria função na service para criar produto pelo nome informado no body.

--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -1,4 +1,4 @@
-const { allProducts, idProducts } = require('../models/productsModel');
+const { allProducts, idProducts, createProduct } = require('../models/productsModel');
 
 const allProductsServices = async (req, res) => {
   const query = await allProducts();
@@ -13,7 +13,14 @@ const idProductServices = async (req, res) => {
   } return res.status(404).json({ message: 'Product not found' });
 };
 
+const createProducServices = async (req, res) => {
+  const { name } = req.body;
+  const query = await createProduct(name);
+  return res.status(201).json(query);
+};
+
 module.exports = {
   allProductsServices,
   idProductServices,
+  createProducServices,
 };
