74bdd57a7823ce38c0bb55fe309a1d8b3364526b
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-09-20 12:37:55 -0300

    Cria rota post para cadastrar produto.

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { allProductsServices, idProductServices } = require('../services/productsServices');
+const {
+  allProductsServices,
+  idProductServices,
+  createProducServices,
+} = require('../services/productsServices');
 
 const router = express.Router();
 
 router.get('/', allProductsServices);
 
 router.get('/:id', idProductServices);
+
+router.post('/', createProducServices);
 
 module.exports = router;
